2066a92762e182dfeef31550d3b8a8fbf2fee49f
Oleg Samoilenko <olaggg-93so@yandex.ru>
2022-11-07 23:10:09 +0300

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+const objSetting = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__item",
+  submitButtonSelector: ".popup__btn",
+  inactiveButtonClass: "popup__btn_inactive",
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__input-error_active",
+};
+
+export {
+  objSetting
+};
