10ef951b9e3c8b6de499abb82e0725c32d6c9902
Oleg Samoilenko <olaggg-93so@yandex.ru>
2023-01-06 20:29:59 +0300

    lll

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
